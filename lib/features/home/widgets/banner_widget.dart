cf786c2c3c4ed699fdf661c2badad58ebac950ad
chifie <levinachifie016@gmail.com>
2026-06-03 11:46:43 +0300

    burger animate

--- lib/features/home/widgets/banner_widget.dart
+++ lib/features/home/widgets/banner_widget.dart
@@ -1,12 +1,39 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/widgets/brand_motion.dart';
 
 /// Restaurant banner widget shown at the top of the Home screen.
-/// Displays a full-width background image with slow rotation animation.
-class BannerWidget extends StatelessWidget {
+/// Shows the burger image (with transparent background) gently floating.
+class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
+
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _floatAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _floatAnimation = Tween<double>(begin: 0, end: -10).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +42,17 @@ class BannerWidget extends StatelessWidget {
       child: Container(
         width: double.infinity,
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        height: 240,
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(24)),
+          gradient: LinearGradient(
+            colors: [
+              AppColors.accentGold.withValues(alpha: 0.12),
+              AppColors.primaryRed.withValues(alpha: 0.06),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
           boxShadow: [
             BoxShadow(
               color: AppColors.darkCharcoal.withValues(alpha: 0.12),
@@ -27,23 +63,25 @@ class BannerWidget extends StatelessWidget {
         ),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(24),
-          child: SizedBox(
-            width: double.infinity,
-            height: 260,
-            child: TweenAnimationBuilder<double>(
-              tween: Tween(begin: 0, end: 2 * math.pi),
-              duration: const Duration(seconds: 20),
-              builder: (context, value, child) {
-                return Transform.rotate(
-                  angle: value,
-                  child: Image.asset(
-                    'lib/assets/images/burgers/background.png',
-                    fit: BoxFit.cover,
-                    errorBuilder: (_, __, ___) => const SizedBox.shrink(),
-                  ),
+          child: Center(
+            child: AnimatedBuilder(
+              animation: _floatAnimation,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(0, _floatAnimation.value),
+                  child: child,
                 );
               },
-              onEnd: () {},
+              child: Image.asset(
+                'lib/assets/images/burgers/background.png',
+                height: 180,
+                fit: BoxFit.contain,
+                errorBuilder: (_, __, ___) => Icon(
+                  Icons.lunch_dining_rounded,
+                  size: 100,
+                  color: AppColors.primaryRed.withValues(alpha: 0.4),
+                ),
+              ),
             ),
           ),
         ),
