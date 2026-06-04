541259aa37618a1ed35eeeb931f567982cd0895a
chifie <levinachifie016@gmail.com>
2026-06-04 09:41:42 +0300

    phone number added

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"glory_burger", origin, size)) {
+  if (!window.Create(L"Glory Burger - 0621490046", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
